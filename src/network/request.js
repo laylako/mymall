672132dba577a256e3aa7eebe14b7a16d50a72aa
lala <minytiny@163.com>
2020-10-30 20:53:24 +0800

    axios实例创建和封装

--- src/network/request.js
+++ src/network/request.js
@@ -1,20 +1,24 @@
 import axios from 'axios'
 
 export function request(config) {
+    // 1. 创建 axios 实例
     const instance = axios.create({
-        baseURL:"http://106.54.54.237:8000/api/wh"||"http://123.207.32.32:8000/api/wh",
+        baseURL: "http://106.54.54.237:8000/api/wh" || "http://123.207.32.32:8000/api/wh",
         timeout: 5000
     })
 
-// 2.axios的拦截器
-// 2.1.请求拦截的作用
+    // 2.axios的拦截器
+    // 2.1.请求拦截的作用
     instance.interceptors.request.use(config => {
         return config
+        // 1. 当 config 中的一些信息不符合服务器的要求时（对请求参数进行序列化）
+        // 2. 当每次发送网络请求时，都希望显示一个 loading
+        // 3. 某些网络请求必须携带一些特殊信息，比如登录时需要携带 token，如果没有 token 跳转到登录界面
     }, err => {
         // console.log(err);
     })
 
-// 2.2.响应拦截
+    // 2.2.响应拦截
     instance.interceptors.response.use(res => {
         return res.data
     }, err => {
--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,10 @@ const Category = () => import("views/category/Category")
 const Cart = () => import("views/cart/Cart")
 const Profile = () => import("views/profile/Profile")
 
-
+// 1. 安装插件（在组件中挂载 $router 属性）（执行插件的 install 方法）
 Vue.use(VueRouter)
 
+// 映射关系：url路径和组件的映射
 const routes=[
     {
         path: '',
@@ -32,9 +33,11 @@ const routes=[
     }
 ]
 
+// 2. 创建 VueRouter 对象
 const router=new VueRouter({
     routes,
     mode: 'history'
 })
 
+// 3. 将 router 对象传入到 vue 实例中（所以要导出，然后在 main.js 里使用）
 export default router
